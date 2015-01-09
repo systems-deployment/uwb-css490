ad6004914b50df1b65652480286c6974475dac38
Morris Bernstein <morris@sweetlabs.com>
2015-01-08 20:02:47 -0800

    Added comments.

--- src/github.com/systems-deployment/uwb-css490/wordcount/wordcount.go
+++ src/github.com/systems-deployment/uwb-css490/wordcount/wordcount.go
@@ -19,7 +19,18 @@ var (
 )
 
 func main() {
+	// Map from word to count
 	counts := make(map[string]int)
+
+	// For each line in standard input, split the line by punctuation
+	// (any non-letter character) and for each word, increment its
+	// count.
+	//
+	// Note how the code takes advantage of the default zero-value for
+	// the map element.
+	//
+	// Note the idiomatic use of underscore to discard unused results
+	// of the Readline function and range operator.
 	buf := bufio.NewReader(os.Stdin)
 	for line, _, err := buf.ReadLine(); err == nil; line, _, err = buf.ReadLine() {
 		words := punct.Split(string(line), -1)
@@ -28,6 +39,9 @@ func main() {
 		}
 	}
 
+	// Stepping through a map: order of elements is indeterminate.
+	// Run this program several times with the same input and note
+	// different output.
 	for w, c := range counts {
 		fmt.Printf("%d\t%s\n", c, w)
 	}
